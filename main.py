031733c4a001a507d857577fb3fc7acc0408876c
chabermehl <clp.habermehl@gmail.com>
2020-11-09 20:19:12 -0700

    Added more audio clips with corresponding commands

--- main.py
+++ main.py
@@ -60,6 +60,57 @@ async def on_ff(ctx):
     """
     user = ctx.author
     helpers.command_logger(user, 'FF')
-    await basic_voice_command(user, 'audio/are-you-tilted.m4a')
+    await basic_voice_command(user, 'audio/FF.mp3')
+
+
+@bot.command(name="unicef", brief="Fred on unicef mission", pass_context=True)
+async def on_unicef(ctx):
+    """
+    Why donate when you can play league?
+    """
+    user = ctx.author
+    helpers.command_logger(user, 'UNICEF')
+    await basic_voice_command(user, 'audio/unicef.mp3')
+
+
+@bot.command(name="rolando", brief="head phone users beware", pass_context=True)
+async def on_rolando(ctx):
+    """
+    No one asked for this
+    """
+    user = ctx.author
+    helpers.command_logger(user, 'ROLANDO')
+    await basic_voice_command(user, 'audio/rolando.mp3')
+
+
+@bot.command(name="lose", brief="THE ONLY WAY TO LOSE IS TO NOT HAVE FUN", pass_context=True)
+async def on_lose(ctx):
+    """
+    A signature Zaiah quote
+    """
+    user = ctx.author
+    helpers.command_logger(user, 'LOSE')
+    await basic_voice_command(user, 'audio/only-way-to-lose.wav')
+
+
+@bot.command(name="stream", brief="Wheres the stream?", pass_context=True)
+async def on_stream(ctx):
+    """
+    Wheres the stream at tho?
+    """
+    user = ctx.author
+    helpers.command_logger(user, 'STREAM')
+    await basic_voice_command(user, 'audio/stream.mp3')
+
+
+@bot.command(name="contribute", brief="Link to GitHub repo. Now accepting PRs")
+async def on_contribute(ctx):
+    """
+    lets people know where the can contribute to the project at
+    """
+    helpers.command_logger(ctx.author, 'CONTRIBUTE')
+    embed = discord.Embed(title="GitHub Repository", description="Now accepting PRs!",
+                          url="https://github.com/chabermehl/titled")
+    await ctx.send("https://github.com/chabermehl/tilted", embed=embed)
 
 bot.run(TOKEN)
